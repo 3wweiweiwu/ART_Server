c2cdb30794ad93c92db6c8094aa2a69ab17ef44b
3wweiweiwu <3wweiweiwu@gmail.com>
2017-06-05 15:58:53 -0400

    modulize vision name validation mechanism

--- controllers/vision/vision.controllers.ARTServer.js
+++ controllers/vision/vision.controllers.ARTServer.js
@@ -120,73 +120,89 @@ exports.putProject=function(req,res,next){
         res.status(400).json(err);
     });
 }
-
+const checkVisionNameValid=function(name,cb=()=>{}){
+    return new Promise((resolve,reject)=>{
+        query={
+            name:name,        
+        }
+        exports.getVision(query)
+        .then((vision)=>{
+            if(vision.length==0){
+                //if no vision found, then return with error
+                err={
+                    result:'error',
+                    status:400,
+                    note:'unable to find vision specified'
+                }
+                reject(err);
+                return cb(err);
+            }
+            else if(vision.length!=1){
+                //if more than 1 vision found, then flag with error
+                err={
+                    result:'error',
+                    status:500,
+                    note:'there are more than one vision with the same name'
+                }
+                reject(err);
+                return cb(err);             
+            }
+            else{
+                resolve(vision);
+                return cb(null,query);
+            }
+        })
+        
+    });
+}
 exports.PutKeyProject=function(req,res,next){
     query={
         name:req.params.vision_name,        
     }
-    exports.getVision(query)
+    checkVisionNameValid(req.params.vision_name)
     .then((vision)=>{
-        if(vision.length==0){
-            //if no vision found, then return with error
-            res.status(400).json({
-                result:'error',
-                note:'unable to find vision specified'
-            });
-        }
-        else if(vision.length!=1){
-            //if more than 1 vision found, then flag with error
-            res.status(500).json({
-                result:'error',
-                note:'there are more than one vision with the same name'
-            });
-        }
-        else{
-            //if there is only 1 vision found, then validate projectBlueprint
-            projectBlueprintModel.findOne({name:req.params.projectBlueprint})
-            .exec((err,blueprint)=>{
-                if(err){
-                    res.status(400).json({
-                        result:'error',
-                        note:err
-                    });
-                }
-                else if(blueprint==null){
-                    //if no blueprint is found, then return error 400
-                    res.status(400).json({
-                        result:'error',
-                        note:'The project blueprint specified is incorrect'
-                    })
-                }
-                else{
-                    //if blueprint is found, then link blueprint with project
-                    vision[0].key_projects.push(blueprint._id);
-                    vision[0].save((err)=>{
-                        if(err)
-                        {
-                            //if error is found, then return error
-                            rs.status(500).json({
-                                result:'error',
-                                note:err
-                            })
-                        }
-                        else{
-                            res.json({
-                                result:'ok'
-                            });
-                        }
-
-                    });
-                }
-                
-                
-                console.log();        
-            });
-
-
-
-        }
-
+        //if there is only 1 vision found, then validate projectBlueprint
         
+        projectBlueprintModel
+        .findOne({name:req.params.projectBlueprint})
+        .exec((err,blueprint)=>{
+            if(err){
+                res.status(400).json({
+                    result:'error',
+                    note:err
+                });
+            }
+            else if(blueprint==null){
+                //if no blueprint is found, then return error 400
+                res.status(400).json({
+                    result:'error',
+                    note:'The project blueprint specified is incorrect'
+                })
+            }
+            else{
+                //if blueprint is found, then link blueprint with project
+                vision[0].key_projects.push(blueprint._id);
+                vision[0].save((err)=>{
+                    if(err)
+                    {
+                        //if error is found, then return error
+                        rs.status(500).json({
+                            result:'error',
+                            note:err
+                        })
+                    }
+                    else{
+                        res.json({
+                            result:'ok'
+                        });
+                    }
+
+                });
+            }
+
+        });
     })
+    .catch(err=>{
+        res.status(err.status).json(err);
+    });
 }
