7688eacde96b65ed51f4057496f8172cf87ff15c
3wweiweiwu <3wweiweiwu@gmail.com>
2017-09-22 13:41:33 -0400

    remove debug flag

--- powershell/VMDeployment/VHD_Detection.ps1
+++ powershell/VMDeployment/VHD_Detection.ps1
@@ -1,7 +1,7 @@
 ﻿$sARTUri='http://mvf1:3000'
 $taskName="VHD Detection"
 $DebugPreference="Continue"
-
+$DebugPreference='SilentlyContinue'
 iex ((New-Object System.Net.WebClient).DownloadString("$sARTUri/api/ps/Library.ps1"))
 iex ((New-Object System.Net.WebClient).DownloadString("$sARTServerUri/api/ps/ARTLibrary.ps1"))
 iex ((New-Object System.Net.WebClient).DownloadString("$sARTUri/api/ps/CommonHeader.ps1"))
