6641f8fc3b7aadbdd76f1979209e345c174f2b3e
3wweiweiwu <3wweiweiwu@gmail.com>
2017-10-16 15:41:24 -0400

    finish code update for apm prestaging and deployment

--- setup/Prestaging/APMDeployment.js
+++ setup/Prestaging/APMDeployment.js
@@ -1,16 +1,17 @@
-let mediaDetection=require('../task/mediaDetection.task.setup.ARTServer');
+let mediaDeployment=require('../vision/deployment.vision.setup.ARTServer');
+//let mediaDetection=require('../task/mediaDetection.task.setup.ARTServer');
 let deployVHD=require('../task/vhdDeployment.task.setup.ARTServer');
-let installMedia=require('../task/installMedia.task.setup.ARTServer');
+//let installMedia=require('../task/installMedia.task.setup.ARTServer');
 let visionSupport = require('../../controllers/vision/support.vision.controllers.ARTServer');
 let projectSupport = require('../../controllers/project/support.project.ARTServer');
 let dormSupport = require('../../controllers/organization/support.dorm.controller.ARTServer');
-let vhdCheckin=require('../task/vhdCheckin.task.setup.ARTServer');
-let prestaging=require('../vision/Prestaging.vision.setup.ARTServer');
+//let vhdCheckin=require('../task/vhdCheckin.task.setup.ARTServer');
+let vhdDetection=require('../task/vhdDetection.task.setup.ARTServer');
 let assert=require('assert');
 describe('Add new vision APM Prestaging',()=>{
 
     it('shall Add APM prestaging into the project',done=>{
-        prestaging.configure(visionSupport.sampleAPMPrestaging,projectSupport.sample_APMMediaDetection,mediaDetection.constant.apm,projectSupport.sampleAPMPrestaging,deployVHD.Constant.apm.prestaging,installMedia.Constant.APM,vhdCheckin.Constant.apmv101,dormSupport.HQDEVBLADE28,'mvt2-apm-01')
+        mediaDeployment.configure(visionSupport.sampleAPMDeployment,projectSupport.sampleAPMVHDDetection,projectSupport.sampleAPMVHDDeployment,deployVHD.Constant.apm.deployment,vhdDetection.Constant.apm,dormSupport.HQDEVBLADE28,[{'vid':'mvt2-apm-d1'}])        
             .then(()=>{
                 done();
             })
